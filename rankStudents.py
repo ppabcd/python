2901e7e1f323c7463deb0cd19bf2ba64e9d3ed4e
ppabcd <rezajuliandri20@gmail.com>
2018-10-27 22:43:52 +0700

    first commit

---
+++ rankStudents.py
@@ -0,0 +1,62 @@
+import operator
+from random import randint
+from collections import Counter
+
+def readStudentDetails():
+    print()
+    # { 'John': 600, 'Ben': 800, 'David': 950, 'Mark': 980}
+    print("Enter the number of students: ")
+    numberOfStudents = int(input())
+    studentRecord = {}
+    for student in range(0, numberOfStudents):
+        print("Enter the name of the student: ")
+        name = input()
+        print("Enter the marks of students: ")
+        marks = int(input())
+        studentRecord[name] = marks
+    print()
+    return studentRecord
+
+
+def rankStudents(studentRecord):
+    try:
+        print()
+        # [('Mark', 980), ('David', 950), ('Ben', 800), ('John', 600)]
+        sortedStudentRecord = sorted(studentRecord.items(), key = operator.itemgetter(1), reverse = True)
+        print("{} has secured first rank, scoring {} marks".format(sortedStudentRecord[0][0], sortedStudentRecord[0][1]))
+        print("{} has secured second rank, scoring {} marks".format(sortedStudentRecord[1][0], sortedStudentRecord[1][1]))
+        print("{} has secured third rank, scoring {} marks".format(sortedStudentRecord[2][0], sortedStudentRecord[2][1]))
+        print()
+        return sortedStudentRecord
+    except IndexError:
+        print("Enter a minimum of 3 students")
+        quit()
+
+
+def rewardStudents(sortedStudentRecord, reward):
+    print()
+    print("{} has received a cash reward of ${}".format(sortedStudentRecord[0][0], reward[0]))
+    print("{} has received a cash reward of ${}".format(sortedStudentRecord[1][0], reward[1]))
+    print("{} has received a cash reward of ${}".format(sortedStudentRecord[2][0], reward[2]))
+    print()
+
+
+def appreciateStudents(sortedStudentRecord):
+    print()
+    for record in sortedStudentRecord:
+        if record[1] >= 950:
+            print("Congratulations on scoring {} marks, {}".format(record[1], record[0]))
+        else:
+            break
+    print()
+
+def lottery(sortedStudentRecord):
+    print("Congratulation {}, you're win lottery.".format(sortedStudentRecord[randint(0, 1)][0]))
+
+
+studentRecord = readStudentDetails()
+sortedStudentRecord = rankStudents(studentRecord)
+lottery(sortedStudentRecord)
+reward = (500, 300, 100)
+rewardStudents(sortedStudentRecord, reward)
+appreciateStudents(sortedStudentRecord)
